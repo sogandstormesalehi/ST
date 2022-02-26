c13f370711e356133e4413fbe2c336cf5c35c2e4
Sogand <sirensa.sa@gmail.com>
2022-02-26 17:22:49 +0330

    first commit

--- style.py
+++ style.py
@@ -20,8 +20,8 @@ import time
 def crop_center(image):
     shape = image.shape
     new_shape = min(shape[1], shape[2])
-    offset_y = max(shape[1] - shape[2], 0) // 2
-    offset_x = max(shape[2] - shape[1], 0) // 2
+    offset_y = max(shape[1] - shape[2], 0) 
+    offset_x = max(shape[2] - shape[1], 0)
     image = tf.image.crop_to_bounding_box(
     image, offset_y, offset_x, new_shape, new_shape)
     return image
@@ -29,21 +29,21 @@ def crop_center(image):
 @functools.lru_cache(maxsize=None)
 def load_image(image_url, image_size=(256, 256), preserve_aspect_ratio=True):
     image_path = tf.keras.utils.get_file(os.path.basename(image_url)[-128:], image_url)
-    img = tf.io.decode_image(
+    image_to_return = tf.io.decode_image(
       tf.io.read_file(image_path),
       channels=3, dtype=tf.float32)[tf.newaxis, ...]
-    img = crop_center(img)
-    img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
-    return img
+    image_to_return = crop_center(image_to_return)
+    image_to_return = tf.image.resize(image_to_return, image_size, preserve_aspect_ratio=True)
+    return image_to_return
 
 def show_n(images, titles=('',)):
-    n = len(images)
+    length = len(images)
     image_sizes = [image.shape[1] for image in images]
-    w = (image_sizes[0] * 6) // 320
-    plt.figure(figsize=(w * n, w))
-    gs = gridspec.GridSpec(1, n, width_ratios=image_sizes)
-    for i in range(n):
-        plt.subplot(gs[i])
+    weight = (image_sizes[0] * 6)
+    plt.figure(figsize=(weight * length, w))
+    gridspecc = gridspec.GridSpec(1, length, width_ratios=image_sizes)
+    for i in range(length):
+        plt.subplot(gridspecc[i])
         plt.imshow(images[i][0], aspect='equal')
         plt.axis('off')
         plt.title(titles[i] if len(titles) > i else '')
